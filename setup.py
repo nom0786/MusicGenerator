cbde5e2a6c3af8e8953b9de8e9d6eb7c79d16bb7
nom0786 <35238228+nom0786@users.noreply.github.com>
2022-12-22 19:37:34 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description='A Python package designed to turn randomy selected notes into a melody based on statistics.',
+    description='A Python package designed to turn randomly selected notes into a melody based on statistics.',
 	url='https://github.com/nom0786/MusicGenerator',
     author='Nyx Zhang & Noman Mohammad',
-    author_email=''
+    author_email='petitmi001@gmail.com'
 )
